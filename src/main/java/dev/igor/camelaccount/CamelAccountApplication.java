754286868cd39a9402b2c0aefe1935e66e0ac328
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2024-01-17 20:46:45 -0300

    start service

---
+++ src/main/java/dev/igor/camelaccount/CamelAccountApplication.java
@@ -0,0 +1,13 @@
+package dev.igor.camelaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/igor/camelaccount/CamelAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.igor.camelaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
